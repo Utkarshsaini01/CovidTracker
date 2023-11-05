788536728b23100493d6dd47300216cbc5e76e82
Utkarsh Saini <sainiutkarsh01@gmail.com>
2023-11-05 22:43:55 +0530

    first commit

---
+++ codes/pamplet.php
@@ -0,0 +1,40 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <header class="MainHeader">
+        <h1 class="MainHeading"> 
+            COVID TRACKER
+        </h1>
+        <nav class="navigator">
+            <ul>
+                <li class="navigations">
+                    <a href="index.html">HOME</a>
+                 </li>
+                <li class="navigations">
+                   <a href="patients_widget.html">PATIENT DETAILS</a>
+                </li>
+                <li class="navigations">
+                   <a href="symptoms_widget.html"> SYMPTOMS</a>
+                </li>
+                <li class="navigations">
+                    <a href="patients_symptomps.html"> PATIENT SYMPTOMS</a>
+                </li>
+                <li class="navigations">
+                    <a href="State_analysis.html">STATE ANALYSIS</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>'
+?>
